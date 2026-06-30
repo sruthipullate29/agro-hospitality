2581d0d4ba8dffb82ea6055fc4edfbc21e9dc8c0
sruthipullate29 <sruthipullate@gmail.com>
2026-06-30 12:22:12 +0530

    fixing server 2

--- src/backend/server.js
+++ src/backend/server.js
@@ -42,15 +42,10 @@ const productStock = {
 };
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
